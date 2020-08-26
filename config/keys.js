6ff5f33e2ff79ee2bc5b7d722b14a22df6bf418f
stevewilkinghof <stevewilkinghoff@gmail.com>
2020-08-26 16:29:58 -0600

    prod vars

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
-module.exports = {
-    googleClientID: '710430024344-k8970i1dvrabv4v9h6t8as4gvm4053eg.apps.googleusercontent.com',
-    googleClientSecret: 'T0qL__kRqSn2amYJh0Qtb5_9',
-    //mongoURI: 'mongodb+srv://SteveAdmin:M4ple5yrup@cluster0.zxeh9.mongodb.net/testdb?retryWrites=true&w=majority'
-    mongoURI: 'mongodb://SteveAdmin:M4ple5yrup@cluster0-shard-00-00.zxeh9.mongodb.net:27017,cluster0-shard-00-01.zxeh9.mongodb.net:27017,cluster0-shard-00-02.zxeh9.mongodb.net:27017/testdb?ssl=true&replicaSet=atlas-3bgqq3-shard-0&authSource=admin&retryWrites=true&w=majority',
+// key.js figure out what set of creds to return
 
-    cookieKey: 'cherrybanana' //note, just random typing of a string
-}
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
+    module.exports = require('./prod');
+    }else{
+        // we are in dev - return the dev set of keys
+        module.exports = require ('./dev');
+    }
 
-//mongodb+srv://SteveAdmin:<password>@cluster0.zxeh9.mongodb.net/<dbname>?retryWrites=true&w=majority
